8a0cee4d2a9a5cf9a5c910d4bdf89e1bba8c468c
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-04-11 18:27:22 +0200

    chore(graphql): Use fragment for invoice fields

--- src/server/lib/graphql.js
+++ src/server/lib/graphql.js
@@ -1,4 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
+import { print } from 'graphql/language/printer';
+import gql from 'graphql-tag';
 
 const getGraphqlUrl = () => {
   const apiKey = process.env.API_KEY;
@@ -12,103 +14,111 @@ const getClient = accessToken => {
   });
 };
 
-const invoiceFields = `
-  slug
-  dateFrom
-  dateTo
-  totalAmount
-  currency
-  year
-  month
-  day
-  host {
-    id
+const invoiceFields = gql`
+  fragment InvoiceFields on InvoiceType {
     slug
-    name
-    image
-    website
-    settings
-    type
-    location {
-      name
-      address
-    }
-    countryISO
-  }
-  fromCollective {
-    id
-    slug
-    name
+    dateFrom
+    dateTo
+    totalAmount
     currency
-    location {
+    year
+    month
+    day
+    host {
+      id
+      slug
       name
-      address
+      image
+      website
+      settings
+      type
+      location {
+        name
+        address
+      }
+      countryISO
     }
-    countryISO
-  }
-  transactions {
-    id
-    createdAt
-    description
-    amount
-    currency
-    type
-    hostCurrency
-    netAmountInCollectiveCurrency
-    taxAmount
     fromCollective {
       id
       slug
       name
+      currency
+      location {
+        name
+        address
+      }
+      countryISO
     }
-    usingVirtualCardFromCollective {
+    transactions {
       id
-      slug
-      name
-    }
-    collective {
-      id
-      slug
-      name
+      createdAt
+      description
+      amount
+      currency
+      type
+      hostCurrency
+      netAmountInCollectiveCurrency
+      taxAmount
+      fromCollective {
+        id
+        slug
+        name
+      }
+      usingVirtualCardFromCollective {
+        id
+        slug
+        name
+      }
+      collective {
+        id
+        slug
+        name
+      }
     }
   }
 `;
 
 export async function fetchInvoiceByDateRange(invoiceInputType, accessToken) {
-  const query = `
-  query InvoiceByDateRange($invoiceInputType: InvoiceInputType!) {
-    InvoiceByDateRange(invoiceInputType:$invoiceInputType) {
-      ${invoiceFields}
+  const query = gql`
+    query InvoiceByDateRange($invoiceInputType: InvoiceInputType!) {
+      InvoiceByDateRange(invoiceInputType: $invoiceInputType) {
+        ...InvoiceFields
+      }
     }
-  }
+
+    ${invoiceFields}
   `;
   const client = getClient(accessToken);
-  const result = await client.request(query, { invoiceInputType });
+  const result = await client.request(print(query), { invoiceInputType });
   return result.InvoiceByDateRange;
 }
 export async function fetchInvoice(invoiceSlug, accessToken) {
-  const query = `
-  query Invoice($invoiceSlug: String!) {
-    Invoice(invoiceSlug:$invoiceSlug) {
-      ${invoiceFields}
+  const query = gql`
+    query Invoice($invoiceSlug: String!) {
+      Invoice(invoiceSlug: $invoiceSlug) {
+        ...InvoiceFields
+      }
     }
-  }
+
+    ${invoiceFields}
   `;
   const client = getClient(accessToken);
-  const result = await client.request(query, { invoiceSlug });
+  const result = await client.request(print(query), { invoiceSlug });
   return result.Invoice;
 }
 
 export async function fetchTransactionInvoice(transactionUuid, accessToken) {
-  const query = `
-  query TransactionInvoice($transactionUuid: String!) {
-    TransactionInvoice(transactionUuid: $transactionUuid) {
-      ${invoiceFields}
+  const query = gql`
+    query TransactionInvoice($transactionUuid: String!) {
+      TransactionInvoice(transactionUuid: $transactionUuid) {
+        ...InvoiceFields
+      }
     }
-  }
+
+    ${invoiceFields}
   `;
 
   const client = getClient(accessToken);
-  const result = await client.request(query, { transactionUuid });
+  const result = await client.request(print(query), { transactionUuid });
   return result.TransactionInvoice;
 }
