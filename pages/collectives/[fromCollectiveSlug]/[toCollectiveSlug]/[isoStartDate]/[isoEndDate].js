4f8870bc405b1da2be8fb10ba196745168d073c6
Oluwaseun Omoyajowo <omoyajowo2015@gmail.com>
2021-03-02 16:52:17 +0100

    fix(paymentReceipts): prevent expense transactions and self-transactions from payments receipts (#580)

--- pages/collectives/[fromCollectiveSlug]/[toCollectiveSlug]/[isoStartDate]/[isoEndDate].js
+++ pages/collectives/[fromCollectiveSlug]/[toCollectiveSlug]/[isoStartDate]/[isoEndDate].js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PDFLayout from '../../../../../components/PDFLayout';
 import PageFormat from '../../../../../lib/constants/page-format';
+import AccountType from '../../../../../lib/constants/account-type';
 import { getAccessTokenFromReq } from '../../../../../lib/req-utils';
 import { fetchInvoiceByDateRange } from '../../../../../lib/graphql/queries';
 import { ReceiptV2 } from '../../../../../components/ReceiptV2';
@@ -13,11 +14,18 @@ class TransactionReceipt extends React.Component {
       const { fromCollectiveSlug, toCollectiveSlug: hostSlug, isoStartDate: dateFrom, isoEndDate } = ctx.query;
       const dateTo = isoEndDate.split('.')[0]; // isoEndDate can include file extension
       const accessToken = getAccessTokenFromReq(ctx);
-      const queryParams = { fromCollectiveSlug, hostSlug, dateFrom, dateTo };
+      const queryParams = { fromCollectiveSlug, hostSlug, dateFrom, dateTo, hasExpense: false };
       const response = await fetchInvoiceByDateRange(queryParams, accessToken, ctx.query.app_key);
 
       if (response.transactions.totalCount > response.transactions.nodes.length) {
         throw new Error('Too many transactions. Please contact support');
+      }
+
+      let transactions = response.transactions.nodes;
+      const fromAccount = response.fromAccount;
+
+      if (fromAccount.type === AccountType.ORGANIZATION) {
+        transactions = transactions.filter((transaction) => transaction.fromAccount.id !== transaction.toAccount.id);
       }
 
       return {
@@ -26,8 +34,8 @@ class TransactionReceipt extends React.Component {
           title: response.host.settings?.invoiceTitle,
           extraInfo: response.host.settings?.invoice?.extraInfo,
           currency: response.host.currency,
-          totalAmount: response.transactions.nodes.reduce((total, t) => total + t.amountInHostCurrency.valueInCents, 0),
-          transactions: response.transactions.nodes,
+          totalAmount: transactions.reduce((total, t) => total + t.amountInHostCurrency.valueInCents, 0),
+          transactions: transactions,
           host: response.host,
           fromAccount: response.fromAccount,
           dateFrom,
