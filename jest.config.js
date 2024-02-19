8bdefc302dd8de3c70a611ca270f8f9296ad8ff3
Benjamin Piouffle <benjamin@opencollective.com>
2024-02-19 10:54:06 +0100

    deps: Remove unused dependencies (#993)

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   transformIgnorePatterns: ['/node_modules/(?!@opencollective/frontend-components)'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
