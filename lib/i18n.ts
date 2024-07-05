93d9de8a7a52dad9296facda08394d74373b92bd
Benjamin Piouffle <benjamin@opencollective.com>
2024-07-05 12:02:23 +0200

    enhancement(TaxForms): surface country on W9

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -3,6 +3,9 @@ import countriesEN from 'i18n-iso-countries/langs/en.json';
 
 countries.registerLocale(countriesEN);
 
-export const getCountryName = (code: string) => {
+export const getCountryName = (code: string): string => {
+  if (!code) {
+    return '';
+  }
   return countries.getName(code, 'en');
 };
